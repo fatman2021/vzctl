f5231794bb03c4044bfbd536bee42372a6b38f4c
Daniel Robbins <drobbins@funtoo.org>
2010-12-26 15:42:45 -0700

    Revert "vps_ip_configure() - simply return 0 if IP_ADDR is null - an attempt to"
    This change didn't have the desired effect, so reverting for now.
    
    This reverts commit b8f0301e2ab694eb66358f57c6c895a8c2bcd300.

--- src/lib/vps_configure.c
+++ src/lib/vps_configure.c
@@ -257,7 +257,7 @@ int vps_ip_configure(vps_handler *h, envid_t veid, dist_actions *actions,
 	const char *root, int op, net_param *net, int state)
 {
 	char *envp[6];
-	char *str = list2str("IP_ADDR", &net->ip);
+	char *str;
 	const char *script = NULL;
 	int ret, i;
 	char vps_state[32];
@@ -265,8 +265,6 @@ int vps_ip_configure(vps_handler *h, envid_t veid, dist_actions *actions,
 	const char *str_state;
 	char *delall = "IPDELALL=yes";
 
-	if (str == NULL)
-		return 0;
 	if (list_empty(&net->ip) && !net->delall && state != STATE_STARTING)
 		return 0;
 	if (actions == NULL)
@@ -293,7 +291,9 @@ int vps_ip_configure(vps_handler *h, envid_t veid, dist_actions *actions,
 	str_state = state2str(state);
 	snprintf(vps_state, sizeof(vps_state), "VE_STATE=%s", str_state);
 	envp[i++] = vps_state;
-	envp[i++] = str;
+	str = list2str("IP_ADDR", &net->ip);
+	if (str != NULL)
+		envp[i++] = str;
 	if (net->delall)
 		envp[i++] = delall;
 	if (net->ipv6_net == YES)
